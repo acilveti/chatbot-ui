d4fce14b01c5bff6af62c3873f5fb5b80730d516
acilveti <ander.cilveti.alvarez@gmail.com>
2023-04-06 10:44:10 +0200

    added authentication page.in progress

---
+++ pages/welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website!</h1>
+      <p>We're glad you're here. Take a look around and enjoy your stay.</p>
+    </div>
+  );
+};
+
+export default Welcome;
